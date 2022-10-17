7f8ac838db2091511032db1c6ab6c635cf41557c
Hyukjin Kwon <hjnim0909@gmail.com>
2022-10-17 17:59:03 +0900

    Basic Types in Kotlin
    - Types can hold different amount of sizes; Byte, Short, Int

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -8,9 +8,14 @@ fun main(){
     // Variable type is redundant but it's useful for the further concept later on
     val name: String
     val age:Int
+    //val myByte: Byte
+    // myByte = 127
+
 
     name = "James"
     age =21
+
+
 
     print("HELLO $name I'm $age years old")
 }
