2367d05f3b30fbf30a21958f484da49c9cc0efa7
Hyukjin Kwon <hjnim0909@gmail.com>
2022-10-17 20:33:59 +0900

    Operators

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -1,27 +1,27 @@
 package com.example.mykotlin
 
 fun main(){
-    //var name = "Kotlin"  -> mutable type: var
-    // val myName = "John"  // , immutable type: val
-    // print("Yo $myName")
-
-    // Variable type is redundant but it's useful for the further concept later on
-    val name: String
-    val age:Int
-    //val myByte: Byte
-    // myByte = 127
-    // val oneLong = 1L   <-- Long can contain the biggest size of bits; by using L after
-
-
-    // val e = 2.7182818284
-    // val eFloat = 2.7182818284
-    // println(e)
-    // println(eFloat)
-
-    name = "James"
-    age =21
-
-
-
-    print("HELLO $name I'm $age years old")
+/**
+ * var - mutable
+ * val - immutable
+ * Best practice - In essence, use val over var
+ *
+ * val name: String = "James" - explicitly initializing a variable
+ *
+ * Another way to initialize variables
+ * val name: String
+ * name = "James"
+ * Basic Types in Kotlin:
+ * (Whole Numbers)
+ *  - Int
+ *  - Byte
+ *  - Short
+ *  Floating Points Types:
+ *   (Decimal Numbers):
+ *    - Double
+ *    - Float
+ *    Note: for precision, use Double.
+ *    val e = 2.7182818284
+val eFloat = 2.7182818284f
+*/
 }
