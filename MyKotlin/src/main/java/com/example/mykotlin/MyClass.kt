40840273fac51aaa2e9b500a9f4e70c92d23509f
Hyukjin Kwon <hjnim0909@gmail.com>
2022-10-17 14:17:33 +0900

    Learning Variables(2) - Variable type -> e.g. val name: String = ""

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -2,6 +2,10 @@ package com.example.mykotlin
 
 fun main(){
     //var name = "Kotlin"  -> mutable type: var
-    val myName = "John"  // , immutable type: val
-    print("Yo $myName")
+    // val myName = "John"  // , immutable type: val
+    // print("Yo $myName")
+
+    // Variable type is redundant but it's useful for the further concept later on
+    val name: String = "James"
+    print("HELLO $name")
 }
