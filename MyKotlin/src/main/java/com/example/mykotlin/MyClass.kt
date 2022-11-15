e98939f1a51655f2a5cd9466240f2a7857fb2ff0
Hyukjin Kwon <hjnim0909@gmail.com>
2022-11-15 19:56:47 +0900

    Functions: Default Arguments

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -2,13 +2,17 @@ package com.example.mykotlin
 
 fun main(){
 
-    calculate(11, 1000, multipleOf = 3)
+//    calculate(11, 1000, message = "is multiple of", multipleOf = 11)
+    calculate(message = "is multiple of", multipleOf = 11)
 }
 
-fun calculate(first: Int, second: Int, multipleOf: Int){
+fun calculate(first: Int = 11,
+              second: Int = 1000,
+              message: String,
+              multipleOf: Int){
     for (i in first..second){
      if(i%multipleOf == 0){
-         println("$i is multiple of $multipleOf")
+         println("$i $message $multipleOf")
      }
     }
 }
