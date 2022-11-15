2312dd61e8e235e1d5e3e0a567cf2c32b9b47985
Hyukjin Kwon <hjnim0909@gmail.com>
2022-11-15 16:00:48 +0900

    Functions: Int Parameter(2)

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -2,13 +2,13 @@ package com.example.mykotlin
 
 fun main(){
 
-    calculate(11, 1000)
+    calculate(11, 1000, multipleOf = 3)
 }
 
-fun calculate(first: Int, second: Int){
+fun calculate(first: Int, second: Int, multipleOf: Int){
     for (i in first..second){
-     if(i%2 == 0){
-         println("$i is multiple of 2")
+     if(i%multipleOf == 0){
+         println("$i is multiple of $multipleOf")
      }
     }
 }
