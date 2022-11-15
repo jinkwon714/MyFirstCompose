a0236a83b6bba416bf8cf540c11262e3c3dc44f5
Hyukjin Kwon <hjnim0909@gmail.com>
2022-11-15 20:15:52 +0900

    Functions: Return Types

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -3,16 +3,23 @@ package com.example.mykotlin
 fun main(){
 
 //    calculate(11, 1000, message = "is multiple of", multipleOf = 11)
-    calculate(message = "is multiple of", multipleOf = 11)
+//    calculate(message = "is multiple of", multipleOf = 11)
+
+    val catAge = calculateCatAge(10) * 10
+    print("This cat is $catAge years old")
 }
 
-fun calculate(first: Int = 11,
-              second: Int = 1000,
-              message: String,
-              multipleOf: Int){
-    for (i in first..second){
-     if(i%multipleOf == 0){
-         println("$i $message $multipleOf")
-     }
-    }
+//fun calculate(first: Int = 11,
+//              second: Int = 1000,
+//              message: String,
+//              multipleOf: Int){
+//    for (i in first..second){
+//     if(i%multipleOf == 0){
+//         println("$i $message $multipleOf")
+//     }
+//    }
+//}
+
+fun calculateCatAge(age: Int):Int{
+    return age *7
 }
