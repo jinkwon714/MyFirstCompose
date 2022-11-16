651a665aa0c526f8f8ac0869ce568c301587fce3
Hyukjin Kwon <hjnim0909@gmail.com>
2022-11-16 14:01:54 +0900

    Lamda Expression - Introduction(2)

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -8,10 +8,13 @@ fun main(){
 
     println(sum(12,10))
 
+    println(catAge(7))
     println(add(12,22))
 }
 
 //val lamdaName : Type = { parameterList -> codeBody}
+
+val catAge: (Int) -> Int = {catAge -> catAge*2}
 val add : (Int, Int) -> Int = {a, b-> a+b}
 
 fun sum(a: Int, b:Int): Int {
