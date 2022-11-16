9db31fb8b10adc709aac1c5335bb9fbeb6c069ec
Hyukjin Kwon <hjnim0909@gmail.com>
2022-11-16 10:48:47 +0900

    Returning a Boolean

--- MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/example/mykotlin/MyClass.kt
@@ -5,21 +5,19 @@ fun main(){
 //    calculate(11, 1000, message = "is multiple of", multipleOf = 11)
 //    calculate(message = "is multiple of", multipleOf = 11)
 
-    val catAge = calculateCatAge(10) * 10
-    print("This cat is $catAge years old")
+    val catAge = calculateCatAge(10)
+    if(checkAge(catAge))
+        print("This cat is old $catAge")
+    else
+        print("This cat is young $catAge")
 }
 
-//fun calculate(first: Int = 11,
-//              second: Int = 1000,
-//              message: String,
-//              multipleOf: Int){
-//    for (i in first..second){
-//     if(i%multipleOf == 0){
-//         println("$i $message $multipleOf")
-//     }
-//    }
-//}
+
 
 fun calculateCatAge(age: Int):Int{
     return age *7
+}
+
+fun checkAge(catAge: Int): Boolean {
+    return catAge > 14
 }
